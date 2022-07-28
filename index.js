285f52a7f88e80cf01e87c3d9111455befc63b4e
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-07-28 14:32:11 -0500

    connection practice

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const Carlos = "carlos";
+const Stacy = "stacy";
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);

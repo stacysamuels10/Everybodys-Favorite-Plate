4d91b35bcb40dc941085a7db6d9b57cd53e2e1f4
CsilvaD99 <Carlos.silva.clss17@gmail.com>
2022-07-29 14:16:29 -0500

    Users Update Passwords

--- backend/database/migrations/20220729182447-Unique-Users2.js
+++ backend/database/migrations/20220729182447-Unique-Users2.js
@@ -1,21 +1,53 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add altering commands here.
      *
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      Email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      Username: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      Password: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add reverting commands here.
      *
      * Example:
      * await queryInterface.dropTable('users');
      */
-  }
+    await queryInterface.dropTable("Users");
+  },
 };

dac2c0fcb29d6de08e96afd49a25132e8dd6d1df
stacy.samuels10@gmail.com <97357702+stacysamuels10@users.noreply.github.com>
2022-08-01 15:09:42 -0500

    finished create a recipe route

--- frontend/client/public/views/js/create-recipe.js
+++ frontend/client/public/views/js/create-recipe.js
@@ -1,7 +1,4 @@
 const createRecipeSubmit = document.getElementById("create-recipe-submit");
-const signInSubmit = document.getElementById("sign-in-submit");
-const changePasswordSubmit = document.getElementById("UP-submit");
-const deleteSubmit = document.getElementById("DEL-submit");
 
 const sendData = async () => {
   const Name = document.getElementById("recipe-name").value;
@@ -9,7 +6,6 @@ const sendData = async () => {
   //const UserID = some sort of sessions id???? think this is on backend when button pressed
   const Ingredients = document.getElementById("ingredients").value;
   const Instructions = document.getElementById("instructions").value;
-  const UserId = 8;
   if (
     Name.length !== 0 &&
     Ingredients.length !== 0 &&
@@ -17,7 +13,6 @@ const sendData = async () => {
   ) {
     const data = {
       Name: Name,
-      UserId: UserId,
       Picture: Picture,
       Ingredients: Ingredients,
       Instructions: Instructions,
@@ -35,106 +30,11 @@ const sendData = async () => {
       }
     );
     const json = await dataWeAreSending.json();
-    console.log(json);
   } else {
     console.log("enter things");
+    //need to make alerts to add something to missing spots
   }
 };
-
-const findUser = async () => {
-  const Username = document.getElementById("SI-username").value;
-  const Password = document.getElementById("SI-password").value;
-  if (Username.length !== 0 && Password.length !== 0) {
-    const data = {
-      Username: Username,
-      Password: Password,
-    };
-    const dataWeAreSending = await fetch(
-      "http://localhost:3000/user/get_user",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
-    const json = await dataWeAreSending.json();
-  } else {
-    console.log("enter");
-  }
-};
-
-const updatePassword = async () => {
-  const Username = document.getElementById("UP-username").value;
-  const OldPassword = document.getElementById("UP-password").value;
-  const NewPassword = document.getElementById("UP-new-password").value;
-  if (
-    Username.length !== 0 &&
-    OldPassword.length !== 0 &&
-    NewPassword.length !== 0
-  ) {
-    const data = {
-      Username: Username,
-      OldPassword: OldPassword,
-      NewPassword: NewPassword,
-    };
-    const dataWeAreSending = await fetch(
-      "http://localhost:3000/user/update_password",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
-    const json = await dataWeAreSending.json();
-  } else {
-    console.log("passwords didnt change");
-  }
-};
-
-const deleteUser = async () => {
-  const Username = document.getElementById("DEL-username").value;
-  const Password = document.getElementById("DEL-password").value;
-  if (Username.length !== 0 && Password.length !== 0) {
-    const data = {
-      Username: Username,
-      Password: Password,
-    };
-    const dataWeAreSending = await fetch(
-      "http://localhost:3000/user/delete_user",
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
-    const json = await dataWeAreSending.json();
-  } else {
-    console.log("enter");
-  }
-};
-
 createRecipeSubmit.onclick = () => {
-  console.log("hello");
   sendData();
-};
-
-signInSubmit.onclick = () => {
-  console.log("i work now");
-  findUser();
-};
-
-changePasswordSubmit.onclick = () => {
-  console.log("hello I am working");
-  updatePassword();
-};
-
-deleteSubmit.onclick = () => {
-  console.log("delete is working");
-  deleteUser();
 };

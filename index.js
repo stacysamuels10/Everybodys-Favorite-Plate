fb1a81c2f89976ff7b080e3e49e61a6e147f604a
CsilvaD99 <Carlos.silva.clss17@gmail.com>
2022-08-02 15:10:25 -0500

    Added the top5 and new5 routes in sorting folder
    the limit right now is 4 because there is only 4 recipes and i was just trying to get it to work, when i test the create new recipe route ill switch it to 5

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const models = require("./backend/database/models");
 const UserRoutes = require("./backend/server/routes/User");
 const NewRecipe = require("./backend/server/routes/NewRecipe");
 const SavedRecipe = require("./backend/server/routes/SavedRecipe");
+const SortingRoutes = require("./backend/server/routes/Sorting");
 
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const store = new SequelizeStore({
@@ -31,6 +32,7 @@ app.use(cors());
 app.use("/user", UserRoutes);
 app.use("/new_recipe", NewRecipe);
 app.use("/saved_recipe", SavedRecipe);
+app.use("/sorting", SortingRoutes);
 
 const CheckLogin = async (req, res, next) => {
   if (req.sesion.user) {

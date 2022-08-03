9a11d2919da8acf002949da6248f2ead40c35425
CsilvaD99 <Carlos.silva.clss17@gmail.com>
2022-08-03 08:53:19 -0500

    res.status on sorting

--- backend/server/routes/Sorting/index.js
+++ backend/server/routes/Sorting/index.js
@@ -5,21 +5,28 @@ const sequelize = require("sequelize");
 
 //don't both of these need try catch? if so, status 200 and 400 needed
 router.get("/top5", async (req, res) => {
-  const top5 = await NewRecipes.findAll({
-    order: [["TimesSaved", "DESC"]],
-    limit: 4,
-  });
-
-  res.send(top5);
+  try {
+    const top5 = await NewRecipes.findAll({
+      order: [["TimesSaved", "DESC"]],
+      limit: 4,
+    });
+    res.status(200).send(top5);
+  } catch {
+    res.status(400).send(error);
+  }
 });
 //
 // try catch needed, status 200 and 400 needed
 router.get("/recently_uploaded", async (rec, res) => {
-  const new5 = await NewRecipes.findAll({
-    order: sequelize.col("createdAt"),
-    limit: 4,
-  });
-  res.send(new5);
+  try {
+    const new5 = await NewRecipes.findAll({
+      order: sequelize.col("createdAt"),
+      limit: 4,
+    });
+    res.send(new5);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 module.exports = router;
